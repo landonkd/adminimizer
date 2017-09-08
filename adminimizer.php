2be4350255f4150cd8621a70d2c6091502976c44
Landon Dorrier <landon.dorrier@gmail.com>
2017-09-08 13:17:39 -0400

    Update adminimizer.php

--- adminimizer.php
+++ adminimizer.php
@@ -14,7 +14,6 @@ Author: Landon Dorrier
 	function remove_menus(){
 	  
 	  remove_menu_page( 'edit-comments.php' );          //Comments
-	  remove_menu_page( 'edit.php' );
 	  
 	}
 	add_action( 'admin_menu', 'remove_menus' );
