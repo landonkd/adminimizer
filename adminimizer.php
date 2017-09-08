9b35bfb338c034bfeae2e02bbc2f661caf3d63ad
Landon Dorrier <landon.dorrier@gmail.com>
2017-09-08 13:04:26 -0400

    Add files via upload

---
+++ adminimizer.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Adminimizer
+Version: 1.1
+License: GPL
+Description: Hide unwanted items from the WordPress admin menu. 
+Author: Landon Dorrier
+*/
+
+/*-----------------------------------------------------------------------------------*/
+/* Remove Unwanted Admin Menu Items */
+/*-----------------------------------------------------------------------------------*/
+
+	function remove_menus(){
+	  
+	  remove_menu_page( 'edit-comments.php' );          //Comments
+	  remove_menu_page( 'edit.php' );
+	  
+	}
+	add_action( 'admin_menu', 'remove_menus' );
+	
+?>
